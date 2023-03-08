ea07cc80022f9258e88f49485e45ad3a24f6b4d4
Erlin011 <127111870+Erlin011@users.noreply.github.com>
2023-03-08 18:04:50 +0530

    Update fibonacci.c

--- c programs/fibonacci.c
+++ c programs/fibonacci.c
@@ -5,18 +5,16 @@ int main()
     int n = 10;
     int a = 0, b = 1;
     
-    // printing the 0th and 1st term
     printf("%d, %d",a,b);
     
-    int nextTerm;
-    
-    // printing the rest of the terms here
-    for(int i = 2; i < n; i++){
-        nextTerm = a + b;
+    int temp;
+    for(int i = 2; i < n; i++)
+    {
+        temp = a + b;
         a = b;
-        b = nextTerm;
+        b = temp;
         
-        printf("%d, ",nextTerm);
+        printf("%d, ",temp);
     }
 
     return 0;
